c659acd89c45cf13b0848d78245943458aabc752
Sergey Zhmailo <sergey.seoz@gmail.com>
2018-06-06 11:53:28 +0300

    060618

--- www/js/hw.js
+++ www/js/hw.js
@@ -1,47 +1,171 @@
-/////////////////          HW7           ////////////////////
-// События
+////////      THIS     ////////////
 // 1.
-document.addEventListener('click', function (evn) {
-    const btn = document.getElementById('btn-msg');
-
-    if (evn.target.id == 'btn-msg') return alert(btn.innerHTML);
-});
-// 2.
-document.addEventListener('mouseover', function (evn) {
-    const btn = document.getElementById('btn-msg');
-
-    if (evn.target.id == 'btn-msg') event.target.style.backgroundColor = "red";
-    else btn.style.backgroundColor = "";
-});
-// 3.
-const result = document.getElementById('tag');
-
-document.addEventListener('click', function (evn) {
-    result.innerHTML = 'Tag: ' + evn.target.tagName;
-});
-
-// Формы
-// 2.
-let arr = [
-    {'лето': 'одень футболку и шорты'},
-    {'осень': 'одень куртку и штаны'},
-    {'зима': 'одень шубу и теплые штаны'},
-    {'весна': 'одень ветровку и джинсы'}
-];
-
-let forma = document.forms['vremena-goda'];
-let sel = forma.elements['sezoni'];
-let inputText = forma.elements['opisanie'];
-let elementName;
-let elementValue;
-
-for (let i = 0; i < arr.length; i++) {
-    elementName = Object.keys(arr[i]);
-    elementValue = Object.values(arr[i]);
-    sel.insertAdjacentHTML('beforeend','<option>'+ elementName+'</option>');
+function getSquare(width, height) {
+    return this.width * this.height;
 }
+const rectangle = {
+    width: 20,
+    height: 10,
+    square: getSquare
+};
+console.log(rectangle.square());
 
-sel.addEventListener('change', function () {
-    inputText.value = Object.values(arr[sel.selectedIndex]);
-});
+// 2.
+function getPrice() {
+    return this.price;
+};
+function getPriceWithDiscount() {
+    return this.price * ((100 - parseFloat(this.discount)) / 100);
+};
 
+const product = {
+    price: 10,
+    discount: '15%'
+};
+res = getPrice.call(product);
+resDisq = getPriceWithDiscount.call(product);
+console.log('Price: ' + res, 'Price with disq: ' + resDisq);
+
+// 3.
+const user = {
+    name: 'Abraham',
+};
+getUserName = function () {
+  return this.name;
+};
+user.getName = getUserName.call(user);
+console.log(user.getName);
+
+// 4.
+function increase() {
+    return this.height += 1;
+}
+const obj4 = {
+    height: 10,
+    increaseParam: increase
+};
+resIncr = obj4.increaseParam();
+console.log(resIncr);
+
+// 5.
+const numeratior = {
+    value: 1,
+    double,
+    pluseOne,
+    minusOne
+};
+function double() {
+    this.value *= 2;
+    return this
+};
+function pluseOne() {
+    this.value += 1;
+    return this
+};
+function minusOne() {
+    this.value -= 1;
+    return this
+}
+res5 = numeratior.double().pluseOne().pluseOne().minusOne();
+console.log(res5.value);
+
+// 6.
+const user6 = {
+    name: 'Abraham'
+};
+const otherUser6 = {
+    name: 'John',
+    getName: function () {
+        return this.name
+    }
+};
+console.log(user6.getName); // undefined потому что у user6 нет этого параметра
+user6.getName = otherUser6.getName; // произошло одалживание функуии
+console.log(user6.getName()); // Получили значение функции из otherUser6 в объекте user6
+console.log(otherUser6.getName()); // Получили значение функции из otherUser6
+
+// THIS задачи 2ч.
+// 1.
+function getList() {
+    return this.list;
+}
+let users = {
+    length: 4,
+    list: ['Abraham', 'James', 'John']
+};
+
+console.log(getList()); // undefined т.к. нет привязки к контексту
+console.log(users.getList = getList); // выведет саму функцию
+console.log(users.getList()); // выполнит функцию с привязкой к контексту юзерс
+console.log(getList.call(users)); // вызывается в контексте юзерс
+
+// 2.
+let products2 = {
+    price: 123,
+    quantity: 5,
+    getSum: getSum
+};
+function getSum() {
+    return this.price * this.quantity
+}
+console.log(products2.getSum());
+
+// 3.
+let products3 = {
+    price: 123,
+    quantity: 5,
+    getSum: getSum
+};
+let details3 = {
+    price: 23,
+    quantity: 6,
+};
+function getSum() {
+    return this.price * this.quantity
+}
+details3.getSum = products3.getSum;
+console.log(details3.getSum());
+
+// 4.
+let sizes = {
+    width: 5,
+    height: 10
+};
+getSquare = function () {
+    return this.width * this.height
+};
+console.log(getSquare.call(sizes));
+
+// 5.
+let numbers = [4, 12, 0, 10, -2, 4];
+min5 = function () {
+    return Math.min.apply(null, this);
+};
+console.log(min5.call(numbers));
+
+// 6.
+const element = {
+    height: '15px',
+    marginTop: '5px',
+    marginBottom: '5px',
+    getFullHeight: function() {
+        return parseFloat(this.height) + parseFloat(this.marginTop) + parseFloat(this.marginBottom);
+    }
+};
+const block = {
+    height: '5px',
+    marginTop: '3px',
+    marginBottom: '3px'
+};
+console.log(element.getFullHeight());
+console.log(element.getFullHeight.call(block));
+
+// 7.
+let element7 = {
+    height: 25,
+    getHeight: function() {
+        return this.height;
+    }
+};
+let getElementHeight = element7.getHeight.bind(element7);
+console.log(getElementHeight());
--- www/js/script.js
+++ www/js/script.js
@@ -1,135 +1,66 @@
-// Task model
-let tasks = [
-    {
-        text: 'Learn js1',
-        id: '1'
-    },
-    {
-        text: 'Learn angular2',
-        id: '2'
-    }
-];
-
-// Get element list-group
-const ul = document.querySelector('.list-group');
-const clearBtn = document.querySelector('.clear-btn');
-const listContainer = document.querySelector('.list-card .card-body');
-const form = document.forms['addTodoItem']; // получили форму
-const input = form['todoText']; // получили инпут из коллекции элементов формы
-const addBtn = document.querySelector('.add-task-btn');
-
-// Events
-clearBtn.addEventListener('click', clearList);
-// clearBtn.removeEventListener('click', clearList);
-form.addEventListener('submit', onSubmit);
-
-input.addEventListener('keyup', function (evn) {
-    addBtn.disabled = !(input.value.length > 2); // если знач в инпуте то снимает дисабл
-});
-
-// вешаем на список ul слушатель с функцией показывать по клику куда кликнули в пределах списка
-ul.addEventListener('click', function (evn) {
-   // console.log(evn.target.classList.contains('delete-task'));
-   if (evn.target.classList.contains('delete-task')) {
-       // console.log('delete'); // если кликнули на корзину, то true
-       const parent = evn.target.closest('li'); // нашли родителя
-       const id = parent.dataset.id; // получаем значение id из нашего атрибута data-id
-       parent.remove(); // удаляем li из разметки
-       // ul.removeChild(li); // 2 вариант удаления через child.
-       deleteTask(id);
-       console.log(tasks);
-
-       if (!tasks.length) showAlertEmpty('Clear list');
-   }
-});
-
-// Generate tasks
-tasks.forEach(task => {
-    let li = listTemplate(task);
-//     // Добавление созданных элементов в список ul
-//     // AppendChild
-//     //ul.appendChild(li); // родительский элемент куда добавляем.appendChild(созданный в create элемент)
-//     // InsertAdjustcentElement
-    ul.insertAdjacentElement("afterbegin", li);
-});
-
-// короткая запись
-// tasks.forEach(task => ul.insertAdjacentElement('afterBegin', listTemplate(task));
-
-// On submit
-function onSubmit(evn) {
-    evn.preventDefault();
-    const value = input.value; // забираем значение из инпута
-    addTask(value); // добавляем новую задачу
+function getItemPrice() {
+    console.log('getprice');
+    return this.currency + this.price;
 }
 
+const item1 = {
+    name: 'Intel i5',
+    price: 200,
+    type: 'processor',
+    currency: '$',
+    getPrice: getItemPrice
+};
+const item2 = {
+    name: 'Intel i7',
+    price: 300,
+    type: 'processor',
+    currency: '$',
+};
 
-function clearList(evn) {
-    evn.preventDefault();
-    // const currentAlert = listContainer.querySelector('.alert'); // получаем текущий алерт
-    // const alert = alertTemplate('Clear List'); // Получили разметку алерта
-    //
-    // if (currentAlert) currentAlert.remove(); // удаляем задвоенное сообщение clear list если есть уже одно
+function convertPrice(currency, coeficient) {
+  return currency + this.price * coeficient;
+};
+////////////////////////////    APPLY     CALL /////////////////////////
+// let res = convertPrice.call(item2, 'EUR', 0.7);
+let res = convertPrice.apply(item1, ['EUR', 0.7]);
+console.log(Math.max.apply(null, [1, 2, 3, 4]));
+////////////////////////////////////////////////////
+function foo2() {
+    const list = [].slice.call(arguments); // из списка аргументов получаем массив
+    console.log(list);
+};
+foo2(2, 43, 12);
+////////////////////////////////////////////////////
 
-    tasks = []; // очистили массив
-    ul.innerHTML = ''; // Удалили разметку
-    showAlertEmpty('Clear list'); // сообщение о пустом списке
+const btn = document.querySelector('.btn');
+//
+// btn.addEventListener('click', function (evn) {
+//     console.log(this);
+//     console.log(evn.target);
+// });
 
-    // listContainer.insertAdjacentHTML('afterBegin', alert); // Добавили алерт в контейнер
-}
-// Show alert empty
-function showAlertEmpty(text) {
-    const alert = alertTemplate(text);
-    const currentAlert = listContainer.querySelector('.alert');
+////////////////////////////       SET TIMEOUT   ////////////////
+// setTimeout(function () {
+//     console.log('timeout');
+// }, 2000);
 
-    if (currentAlert) currentAlert.remove();
-    listContainer.insertAdjacentHTML('afterBegin', alert);
-    return 'success';
-}
-// Alert Template
-function alertTemplate(text) {
-    const template = `  
-        <div class="alert alert-info">${text}</div>
-    `; // создаем шаблонную строку
-    return template;
-}
+// setTimeout(item1.getPrice, 2000);
+
+/////////////////////////         BIND        /////////////////
+// let getPriceBind = item1.getPrice.bind(item1); // контекст привязан жестко к item1
+
+// setTimeout(getPriceBind, 1000);
+
+// btn.addEventListener('click', item1.getPrice.bind(item1));
+// btn.addEventListener('click', () => item1.getPrice()); // короткая запись
 
 
-// Task List Template
-function listTemplate(task) {
-    // Create element
-    const li = document.createElement('li');
-    const deleteIcon = `<i class="fas fa-trash delete-task"></i>`; // создали разметку для иконки удаления
-
-    // Add text of task into li
-    li.textContent = task.text;
-    li.insertAdjacentHTML('beforeend', deleteIcon); // добавили иконку в ли
-    // Set id of task into li
-    li.setAttribute('data-id', task.id);
-    // Add class
-    li.classList.add('list-group-item');
-    // Return created element
-    return li;
-}
-
-// Add task
-function addTask(text) {
-    // Create task object
-    const newTask = {text, id: String(tasks.length + 1)};
-    // Add task on tasks
-    tasks.unshift(newTask);
-    // Add li on ul
-    ul.insertAdjacentElement("afterbegin", listTemplate(newTask));
-}
-// Delete task
-function deleteTask(id) {
-    for (let i = 0; i < tasks.length; i++) {
-        if (id == tasks[i].id) {
-            tasks.splice(tasks[i], 1); // удаление по id из массива
-        }
-    }
-    // ul.innerHTML = ''; // очистили html
-    // tasks.sort((prev, next) => prev.id - next.id); // упорядочили элементы массива по id
-    // tasks.forEach(task => ul.insertAdjacentElement("afterbegin", listTemplate(task))); // создали новую разметку
-}
+//////////////////////////       SET INTERVAL       ////////////////////////
+// let i = 0;
+// let interval = setInterval(function () {
+//     console.log(i++);
+// }, 1000);
+// btn.addEventListener('click', function (evn) {
+//     clearInterval(interval); // stop
+// });
 
